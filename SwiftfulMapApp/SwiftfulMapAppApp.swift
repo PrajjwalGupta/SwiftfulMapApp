54bbfb78d0cac89145fac57a9a496bf0a83fcd78
Prajjwal Gupta <31537066+PrajjwalGupta@users.noreply.github.com>
2022-06-03 19:39:58 +0530

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Prajjawal Gupta on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
